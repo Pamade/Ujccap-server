e4caff12e10ab32f48660fe0a341fdba26bd11d3
Pamade <insune15@gmail.com>
2023-02-11 15:47:57 +0100

    add cors

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ const opinions = require("./routes/opinions");
 const anyUser = require("./routes/anyUser");
 const port = process.env.PORT || 5001;
 const BASE_API = "/api/v1/";
+const cors = require("cors");
 
+const corsOptions = {
+  origin: "https://ujccap-client-mg4y3kilp-pamade.vercel.app",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static(__dirname));
 app.use(`${BASE_API}auth`, userRouter);
