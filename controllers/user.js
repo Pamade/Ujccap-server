eb64d5516721a152c0b702ed26302c7ad6a5d84d
Pamade <insune15@gmail.com>
2023-02-11 17:54:41 +0100

    add bcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const { User } = require("../models/user");
 const sendEmail = require("../utils/sendEmail");
 const { isEmail } = require("validator");
@@ -118,8 +118,8 @@ const login = async (req, res, next) => {
     else if (user.isGoogleUser) {
       return res.status(400).json({ err: "You have to login via google" });
     }
-    // await bcrypt.compare(password, user.password);
-    const decryptedPassword = false;
+
+    const decryptedPassword = await bcrypt.compare(password, user.password);
 
     if (!decryptedPassword)
       return res.status(401).json({ err: INVALID_CREDENTIALS });
@@ -253,10 +253,10 @@ const changePassword = async (req, res) => {
   try {
     const { password, repeatPassword, newPassword } = req.body;
     const { id } = req.user;
-    // await bcrypt.compare(password, user.password);
-    const user = false;
 
-    const isPasswordCorrect = false;
+    const user = await User.findById(id);
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
     if (!isPasswordCorrect) {
       return res.status(401).json({ err: "Incorrect password" });
     } else if (isPasswordCorrect && password === repeatPassword) {
@@ -281,8 +281,8 @@ const changeEmail = async (req, res) => {
     const { id } = req.user;
     const user = await User.findById(id);
     const { newEmail, password } = req.body;
-    // await bcrypt.compare(password, user.password);
-    const isPasswordCorrect = false;
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
     if (!isPasswordCorrect) {
       return res.status(401).json({ err: "Incorrect password" });
     }
